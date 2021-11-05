57d7ec6f19a75b701032affe2124d523baed9c64
w9hdg <w9hdg.p@gmail.com>
2021-11-05 08:07:41 -0500

    Update kderice-restore.sh

--- kderice-restore.sh
+++ kderice-restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH=$PATH:~/.local/bin
-cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
+cp -r $HOME/ArchHodag/dotfiles/* $HOME/.config/
 pip install konsave
 konsave -i $HOME/ArchHodag/kde.knsv
 sleep 1
