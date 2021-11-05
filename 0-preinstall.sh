89e9d21c9bffe43d75239dd7b5060a6abcf4df03
w9hdg <w9hdg.p@gmail.com>
2021-11-05 06:33:11 -0500

    ChaoticAUR

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -16,6 +16,14 @@ timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib terminus-font
 setfont ter-v22b
 sed -i 's/^#Para/Para/' /etc/pacman.conf
+#Enable ChaoticAUR for initial install
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+echo -e "\nInstalling Base System\n" >> /etc/pacman.conf
+pacman -Sy --noconfirm
 pacman -S --noconfirm reflector rsync grub
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo -e "-------------------------------------------------------------------------"
